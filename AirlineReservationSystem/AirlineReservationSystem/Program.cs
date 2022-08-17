3a6861c7f4c0cb846cf2eeaf6316afa52052cb5f
Shashanksai150 <59482415+Shashanksai150@users.noreply.github.com>
2022-08-17 11:46:28 +0530

    Update Program.cs

--- AirlineReservationSystem/AirlineReservationSystem/Program.cs
+++ AirlineReservationSystem/AirlineReservationSystem/Program.cs
@@ -16,7 +16,7 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddDbContext<HawksAvaitionDBContext>(options =>
 {
     options.UseSqlServer(
-        connectionString: "server=SHARKTOP-96\\SQLEXPRESS;database=HawksAviationDb;Trusted_Connection=True" //builder.Configuration["ConnectionString:HawksAviationDB"]
+        connectionString: "server=*********************;database=HawksAviationDb;Trusted_Connection=True" //builder.Configuration["ConnectionString:HawksAviationDB"]
     ); 
 });
 
